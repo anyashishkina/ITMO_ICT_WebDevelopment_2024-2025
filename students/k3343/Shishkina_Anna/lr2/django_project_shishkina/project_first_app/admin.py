6a8765fd18b6e011caffbe75350b5a546c0ffedd
annashishkina <shishkina_anna04@mail.ru>
2024-11-04 23:15:20 +0300

    изменено отображение информации в админке

--- students/k3343/Shishkina_Anna/lr2/django_project_shishkina/project_first_app/admin.py
+++ students/k3343/Shishkina_Anna/lr2/django_project_shishkina/project_first_app/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import UserProfile, Homework,Submission
 
-admin.site.register(UserProfile)
-admin.site.register(Submission)
-admin.site.register(Homework)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'role')
 
 
+@admin.register(Homework)
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'teacher', 'issue_date', 'due_date', 'description', 'penalty_info')
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('hw', 'student', 'submission_text', 'grade')
