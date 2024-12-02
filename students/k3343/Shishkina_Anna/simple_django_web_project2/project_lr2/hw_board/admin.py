e0036f505ab3970e95726377b9dcf20a2d2c6143
annashishkina <shishkina_anna04@mail.ru>
2024-12-02 23:18:36 +0300

    добавлены практические работы 3.1 и 3.2 + лаб 3

--- students/k3343/Shishkina_Anna/simple_django_web_project2/project_lr2/hw_board/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Subject, Hw
-
-admin.site.register(Subject)
-admin.site.register(Hw)
--- students/k3343/Shishkina_Anna/simple_django_web_project2/project_lr2/hw_board/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HwBoardConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "hw_board"
